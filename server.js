38012667045dcf4819652340a1211ee81a99cd61
ganakivi <ganakivi@gmail.com>
2017-08-15 08:17:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/article-one', function (req,res) {
+    res.send('this is article one which is requested');
+});
+app.get('article-two', function (req,res){
+    res.send('tnis is the second article which is requested');
+});
+app.get('article-three', function (req,res){
+    res.send('tnis is the third article which is requested');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
