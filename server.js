54f0ff8ca5b5ee3b2e970e95a4498048fe2f9f07
ganakivi <ganakivi@gmail.com>
2017-08-18 19:47:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,61 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne = {
+    title :'article one | viki',
+    heading : 'ARTICLE ONE',
+    content : `            <H3>i tried to add some articles</H3>
+            <br>
+            <hr>
+            <div><p>
+            it is the best thing with a little improvement in my experience. there is nothing wrong in trying this. i normally won't get angry.
+            But in fact there is no situations that i faced made me angry that is the truth. i have not faced any difficult situations.
+            but if some situations lead to angry there is a feel always come that this is not the correct situation. and a lecture begins in my
+            mind. this help in solving </p>
+            </div>
+            <div><p>
+            it is the best thing with a little improvement in my experience. there is nothing wrong in trying this. i normally won't get angry.
+            But in fact there is no situations that i faced made me angry that is the truth. i have not faced any difficult situations.
+            but if some situations lead to angry there is a feel always come that this is not the correct situation. and a lecture begins in my
+            mind. this help in solving </p>
+            </div>
+            <div><p>
+            it is the best thing with a little improvement in my experience. there is nothing wrong in trying this. i normally won't get angry.
+            But in fact there is no situations that i faced made me angry that is the truth. i have not faced any difficult situations.
+            but if some situations lead to angry there is a feel always come that this is not the correct situation. and a lecture begins in my
+            mind. this help in solving</p> 
+            </div>`
+};
+function create(data){
+var template=    `<!doctype html>
+<html>
+    <head>
+        <title>
+            ${data.title}
+        </title>
+        <meta name="viewport" content="width=divice-width, initial-scale=1">
+        <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+                <A href="/">home</A>
+            </div>
+            <div>
+                <h1><center>${data.heading}</center></h1>
+            </div>
+            ${data.content}
+        </div>
+    </body>
+</html>`
+return template;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req,res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(path.join(create(articleOne)));
 });
 app.get('/article-two', function (req,res){
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
