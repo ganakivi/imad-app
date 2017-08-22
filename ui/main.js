26641d9482f182b9cbd8dda3aa85a787e2f2d4ec
ganakivi <ganakivi@gmail.com>
2017-08-22 15:52:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,8 @@ element.innerHTML = 'this is the new Text';
 var img = document.getElementById('image');
 var marginval= 0;
 function moveleft(){
- img.style.marginLeft = marginval++;
+    marginval++;
+ img.style.marginLeft = marginval+ 'px';
 }
 img.onclick = function (){
     var interval = setInterval(moveleft, 30);
