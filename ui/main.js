a26947484ca7820c9576fb026b78c88f8a0d4b43
ganakivi <ganakivi@gmail.com>
2017-09-19 10:22:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,17 @@
+var bn=document.getElementById('click');
+bn.onckick = function(req,res){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function (){
+        if(request.readyState === XMLHttpRequst.DONE)
+        if(request.status === 200){
+            var counter= request.responseTest;
+            var span = document.getElementById('clicked');
+            span.innerHTML = counter.toString();
+        }
+    };
+};
+resuest.open('GET','http://ganakivi.imad.hasura-app.io/counter',true);
+request.send(null);
 var img = document.getElementById('image');
 var marginval= 0;
 var increment=1;
